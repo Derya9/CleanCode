bc5b0d8ecf9fbf4e882e6e7fe054cfda5418f8fa
derya <d.k.gesierich@gmail.com>
2024-03-06 11:42:45 +0100

    implemented mentor feedback

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php 
     function extractNumbersFromString($givenString) : array
     {
-        $extractedNumbers = array();
-        $explodedString = explode(" ", $givenString);
+        $extractedNumbers = [];
+        $splitStrings = explode(" ", $givenString);
 
-        foreach($explodedString as $string) {
-            if(is_numeric($string)) {
+        foreach($splitStrings as $string) 
+        {
+            if(is_numeric($string)) 
+            {
                 array_push($extractedNumbers, $string);
             }
         }
         return $extractedNumbers;
     }
 
-    function transformCharactersToNumbers($numbers) : array
+    function transformNumbersToCharacters($numbers) : array
     {
-        // TODO: receive array of numbers (ascii values) transform them to characters and return as array
-        $ascii = [];
-        foreach ($numbers as $number) {
-            array_push($ascii, $chr($number));
+        $characters = [];
+        foreach ($numbers as $number) 
+        {
+            array_push($characters, chr($number));
         }
-        return $ascii;
+        return $characters;
     }
